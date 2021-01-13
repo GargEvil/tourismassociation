13f76711c8fcdb40d86b9a05846b9e064e3266b2
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-13 21:33:36 +0100

    Initial commit
    Creating a project using SpringInitializr
    Connecting to DB

---
+++ src/main/java/com/tourism/tourismassociation/TourismAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.tourism.tourismassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourismAssociationApplication.class, args);
+    }
+
+}
