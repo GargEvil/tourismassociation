81b409597e3ca22f6987071618b9f8db5172abb5
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-14 19:19:05 +0100

    Updating UserService
    Running all previous tests,
    they are passing

--- src/main/java/com/tourism/tourismassociation/service/UserService.java
+++ src/main/java/com/tourism/tourismassociation/service/UserService.java
@@ -1,15 +1,21 @@
 package com.tourism.tourismassociation.service;
 
 import com.tourism.tourismassociation.model.User;
+import com.tourism.tourismassociation.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    public List<User> findAll(){ return new ArrayList<>(); }
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){ return userRepository.findAll(); }
 
     public User save(User user){ return  new User(); }
 
