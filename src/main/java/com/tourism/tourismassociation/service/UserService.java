28447d8082433d0571e62769fca3bb1f096c279a
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-14 19:59:50 +0100

    Implementing logic for saveUser
    saveUser test pass

--- src/main/java/com/tourism/tourismassociation/service/UserService.java
+++ src/main/java/com/tourism/tourismassociation/service/UserService.java
@@ -17,6 +17,6 @@ public class UserService {
 
     public List<User> findAll(){ return userRepository.findAll(); }
 
-    public User save(User user){ return  new User(); }
+    public User save(User user){ return  userRepository.save(user); }
 
 }
