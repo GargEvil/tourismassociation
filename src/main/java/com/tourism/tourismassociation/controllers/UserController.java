e474e7191a8e2c1b5adf2c3cff45fbdf7006d412
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-14 12:26:00 +0100

    Adding impl for CreatingUser
    Creating Post method in Controller for
    adding User

--- src/main/java/com/tourism/tourismassociation/controllers/UserController.java
+++ src/main/java/com/tourism/tourismassociation/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,4 +23,12 @@ public class UserController {
     {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
+
+
+    @PostMapping("/users")
+    ResponseEntity<User> CreateUser(@RequestBody User user)
+    {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
 }
