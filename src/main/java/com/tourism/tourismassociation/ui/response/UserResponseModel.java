2658a9831a54dd0e180816017eab98c6e9d3fe3f
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-21 20:23:22 +0100

    Introducing RequestModel and ResponseModel v2

---
+++ src/main/java/com/tourism/tourismassociation/ui/response/UserResponseModel.java
@@ -0,0 +1,13 @@
+package com.tourism.tourismassociation.ui.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseModel {
+
+    private String userId;
+
+    private String email;
+}
