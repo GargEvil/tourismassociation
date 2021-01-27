2cc5207805f4af67d0e33783c1c216bcb67f0b7c
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-27 13:56:20 +0100

    Implementing findLandmarkByKeyword(keyword) in LandmarkRepository
    running test, it passes

--- src/main/java/com/tourism/tourismassociation/repository/LandmarkRepository.java
+++ src/main/java/com/tourism/tourismassociation/repository/LandmarkRepository.java
@@ -2,10 +2,17 @@ package com.tourism.tourismassociation.repository;
 
 import com.tourism.tourismassociation.model.Landmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LandmarkRepository extends JpaRepository<Landmark, Long> {
 
     Landmark findByName(String name);
+
+    @Query(value="select * from Landmark l where l.name LIKE %:keyword%",nativeQuery=true)
+    List<Landmark> findLandmarksByKeyword(@Param("keyword") String keyword);
 }
