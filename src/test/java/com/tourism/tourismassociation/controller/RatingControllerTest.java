c0b5b6b927138975db25e5a1e7a6384a625c870e
Mahir Skula <mahir.skula96@hotmail.com>
2021-01-28 12:41:25 +0100

    Creating getAllRating test
    I step: [red] - green - refactor

--- src/test/java/com/tourism/tourismassociation/controller/RatingControllerTest.java
+++ src/test/java/com/tourism/tourismassociation/controller/RatingControllerTest.java
@@ -1,12 +1,20 @@
 package com.tourism.tourismassociation.controller;
 
 import com.tourism.tourismassociation.service.RatingService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
 
 @WebMvcTest
 @ExtendWith(SpringExtension.class)
@@ -17,4 +25,22 @@ public class RatingControllerTest {
 
     @MockBean
     private RatingService ratingService;
+
+    @Test
+    @DisplayName(" Get /ratings - SUCCESS")
+    void getAllRatings() throws Exception {
+        //arrange
+        List<RatingDTO> ratingList = new ArrayList<>();
+        ratingList.add(new RatingDTO(1, 1, "Prelijep osjecaj je biti na ovoj lokaciji u centru Sarajeva."));
+        ratingList.add(new RatingDTO(1, null, "Na ljeto sam posjetio Sarajevo, i nezaobilaznu Bascarsiju, uzivao sam svaki trenutak."));
+        when(ratingService.findAll()).thenReturn(ratingList);
+
+        //act
+        mockMvc.perform(MockMvcRequestBuilders.get("/ratings")
+                .contentType(MediaType.APPLICATION_JSON))
+                //assert
+                .andExpect(jsonPath("$", hasSize(2))).andDo(print());
+
+
+    }
 }
